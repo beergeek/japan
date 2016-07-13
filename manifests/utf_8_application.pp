d63c4b0e0bc22b13a4f362bf036fcf138461adea
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-07-13 12:31:58 +1000

    Removed all deps

--- manifests/utf_8_application.pp
+++ manifests/utf_8_application.pp
@@ -3,7 +3,7 @@ application utf_8::utf_8_application (
 ) {
   utf_8::component_one { "${name}_first":
     message => $message,
-    export  => Blank["exported_${message}"],
+    #export  => Blank["exported_${message}"],
   }
 
   utf_8::component_one { "${name}_second":
