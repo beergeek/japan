69418dea721a7b626f7bd595da816223fa047aab
beergeek <brett.gray@puppetlabs.com>
2016-08-22 12:14:42 +1000

    trigger puppet for nrp users

--- manifests/puppet_users.pp
+++ manifests/puppet_users.pp
@@ -7,6 +7,12 @@ class utf_8::puppet_users (
     fail("This class is only for Linux nodes")
   }
 
+  Pe_ini_setting {
+    path    => "/home/${user_key}/.puppetlabs/etc/puppet/puppet.conf",
+    section => 'agent',
+    require => File["/home/${user_key}/.puppetlabs/etc/puppet/puppet.conf"],
+  }
+
   $user_hash.each |String $user_key, Hash $user_values| {
     if $user_values['manage_user'] == true {
       user { $user_key:
@@ -39,26 +45,28 @@ class utf_8::puppet_users (
 
     pe_ini_setting { "${user_values['ascii_name']}_certificate":
       ensure  => present,
-      path    => "/home/${user_key}/.puppetlabs/etc/puppet/puppet.conf",
-      section => 'agent',
       setting => 'certname',
       value   => "${user_values['ascii_name']}_${::hostname}",
     }
 
     pe_ini_setting { "${user_values['ascii_name']}_user":
       ensure  => present,
-      path    => "/home/${user_key}/.puppetlabs/etc/puppet/puppet.conf",
-      section => 'agent',
       setting => 'user',
       value   => $user_key,
     }
 
     pe_ini_setting { "${user_values['ascii_name']}_server":
       ensure  => present,
-      path    => "/home/${user_key}/.puppetlabs/etc/puppet/puppet.conf",
-      section => 'agent',
       setting => 'server',
       value   => $::settings::server,
+    }
+
+    cron { "${user_key}_puppet":
+      ensure  => present,
+      command => '/opt/puppetlabs/bin/puppet agent -t',
+      minute  => '*/10',
+      user    => $user_key,
+      require => [User[$user_key],Pe_ini_setting["${user_values['ascii_name']}_certificate","${user_values['ascii_name']}_user","${user_values['ascii_name']}_server"]],
     }
   }
 }
