50e78e099f26398b4276dec404ab1507514b3d10
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-09-07 15:59:11 +1000

    Fixing functions

--- functions/puppet.pp
+++ functions/puppet.pp
@@ -1,5 +1,5 @@
 function utf_8::puppet(
-  $input = 'ウェブサーバー',
+  $input = 'アパッチ族',
 ) {
   "${input} puppet function works"
 }
--- lib/puppet/parser/functions/utf_8_ruby.rb
+++ lib/puppet/parser/functions/utf_8_ruby.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 module Puppet::Parser::Functions
   newfunction(:utf_8_ruby, :type => :rvalue) do |args|
+    value = args[0]
     value ||= 'アパッチ族'
     "#{value} ruby function works"
   end
